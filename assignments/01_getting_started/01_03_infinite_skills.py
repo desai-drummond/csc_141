84193e2e5c06d8961c034d0bc032aab31e086e70
desai-drummond <desai.drummond001@albright.edu>
2026-09-10 12:05:22 -0400

    test commit 2

--- assignments/01_getting_started/01_03_infinite_skills.py
+++ assignments/01_getting_started/01_03_infinite_skills.py
@@ -1,5 +1,8 @@
 '''
 I need infinite skills to be a great programmer.
 I want to get better at becoming a great programmer to build my own business. 
+I want to create programs that can help people and make their lives easier.
+I am excited to keep learning and turning my ideas into reality.
+In the future, I hope to build a successful career in technology.
 
 '''
