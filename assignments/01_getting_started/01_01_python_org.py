b9188eb75378dcd38ce24cf8814e422e8e8e77f9
desai-drummond <desai.drummond001@albright.edu>
2026-09-10 10:57:12 -0400

    1st update

--- assignments/01_getting_started/01_01_python_org.py
+++ assignments/01_getting_started/01_01_python_org.py
@@ -1,4 +1,5 @@
 # The website is very simple and has very cool features for programming and coding. 
 # It's a great resource for learning Python and improving your programming skills.
-
-
+# The website provides tutorials, documentation, and community support for Python developers.
+# You can add your own code snippets, ask questions, and share your knowledge with others in the Python community.
+# There is math you can do on the website, and you can also find information about Py games.
