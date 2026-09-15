ad67c169f4fa0f9e5e5de1c820db38a6fb9a8eeb
desai-drummond <desai.drummond001@albright.edu>
2026-09-15 12:05:08 -0400

    test 2.6

---
+++ assignments/02_variables_and_data_types/02_06_famous_quote2.py
@@ -0,0 +1,6 @@
+# Desai Drummond
+# Chapter 2
+
+famous_person = "Colin Powell"
+message = f'"{famous_person} once said, "There are no secrets to success. It is the result of preparation, hard work, and learning from failure.""'
+print(message)
