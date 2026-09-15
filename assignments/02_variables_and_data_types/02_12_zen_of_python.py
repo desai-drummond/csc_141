c04891611f0fe65bbf6fc5083c599e57e89a946c
desai-drummond <desai.drummond001@albright.edu>
2026-09-15 12:50:53 -0400

    test 2-12.

---
+++ assignments/02_variables_and_data_types/02_12_zen_of_python.py
@@ -0,0 +1,10 @@
+# Desai Drummond
+# Chapter 2
+
+import this 
+
+# Zen of Python - a few principles I found interesting are: 
+# Simple is better than complex 
+# Readability counts.
+# Now is better than never.
+# If the implementation is hard to explain, it's a bad idea.
