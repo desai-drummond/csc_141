212084936084dafc782179bcf3f814fc5bbc2694
desai-drummond <desai.drummond001@albright.edu>
2026-09-15 12:45:19 -0400

    test 2-11.

---
+++ assignments/02_variables_and_data_types/02_11_adding_comments.py
@@ -0,0 +1,18 @@
+# Desai Drummond
+# Chapter 2
+# This stores my favorite number in a variable and prints it out with a message.
+
+
+favorite_number = 12
+print(f"My favorite number is {favorite_number} because it's the number of months in a year and my uncle wore {favorite_number} for college football too.")
+print(favorite_number)
+
+
+# This program prints four different ways to calculate the number 8.
+print(2 + 6)
+print(16 - 8)
+print(1 * 8)
+print(64 / 8)   
+
+# September 15, 2026
+# Tuesday Computer Science 141
