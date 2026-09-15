d157b359dee41b71a5bf81f2f9fee77c97bdfaa9
desai-drummond <desai.drummond001@albright.edu>
2026-09-15 12:36:50 -0400

    test 2-10.

---
+++ assignments/02_variables_and_data_types/02_10_favorite_number.py
@@ -0,0 +1,6 @@
+# Desai Drummond
+# Chapter 2
+
+favorite_number = 12
+print(f"My favorite number is {favorite_number} because it's the number of months in a year and my uncle wore {favorite_number} for college football too.")
+print(favorite_number)
