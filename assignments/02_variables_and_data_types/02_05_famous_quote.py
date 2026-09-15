b6d23862ba646590a3485b7d167acbdb1564b58c
desai-drummond <desai.drummond001@albright.edu>
2026-09-15 11:56:37 -0400

    test

---
+++ assignments/02_variables_and_data_types/02_05_famous_quote.py
@@ -0,0 +1,6 @@
+# Desai Drummond
+# Chapter 2
+
+famous_person = "Colin Powell"
+quote = "Sucess is the result of perfection, hard work, learning from failure, loyalty, and persistence."
+print(f'{famous_person} once said, "{quote}"')
