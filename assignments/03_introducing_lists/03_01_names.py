d9a8ab6a76ec17aa7b7551889a4e977c95cbf4d1
desai-drummond <desai.drummond001@albright.edu>
2026-09-15 13:07:15 -0400

    test 3-1.

---
+++ assignments/03_introducing_lists/03_01_names.py
@@ -0,0 +1,19 @@
+# Desai Drummond 
+# Chapter 3
+
+'''
+
+let's do list 
+
+
+'''
+
+
+
+
+
+names = ["Desai", "Colton", "Kaul", "Marcello",] 
+print(names[0])
+print(names[1])
+print(names[2])
+print(names[3])
