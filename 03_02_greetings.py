0f88feb771cc3cc523817ae59b12d407b91c91be
desai-drummond <desai.drummond001@albright.edu>
2026-09-15 13:16:05 -0400

    test 3-2.

---
+++ 03_02_greetings.py
@@ -0,0 +1,15 @@
+# Desai Drummond
+# Chapter 3
+
+'''
+
+let's do greetings
+
+'''
+
+
+names = ["Desai", "Colton", "Kaul", "Marcello"]
+print(f"Hello {names[0]}, Desai how are you?")
+print(f"Hello {names[1]}, Greetings Colton how are you feeling?")
+print(f"Hello {names[2]}, Good to see you Kaul how is your day going?")
+print(f"Hello {names[3]}, Hello Marcello how are you?")
