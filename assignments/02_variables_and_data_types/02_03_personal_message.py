ed285187e7b1313abfb3955259e3cf9bebd10baa
desai-drummond <desai.drummond001@albright.edu>
2026-09-15 11:32:01 -0400

    test

---
+++ assignments/02_variables_and_data_types/02_03_personal_message.py
@@ -0,0 +1,9 @@
+# Desai Drummond
+# Chapter 2
+
+name = "Desai Drummond"
+message = f"Hello {name}, would you like to learn some Python today?"
+print(message)  
+age = 18
+school = "Albright College"
+print(f" I am 18 years old and I go to Albright College.")
