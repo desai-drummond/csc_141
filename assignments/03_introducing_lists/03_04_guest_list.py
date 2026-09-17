51f83c068f7e971d0ef804b38d7cbc9dfec0dba0
desai-drummond <desai.drummond001@albright.edu>
2026-09-17 14:23:08 -0400

    test 3-4.

---
+++ assignments/03_introducing_lists/03_04_guest_list.py
@@ -0,0 +1,9 @@
+# Desai Drummond
+# Chapter 3
+
+guests = ["Lebron James", "Michael Jordan", "Kobe Bryant", "Shaquille O'Neal"]
+
+print(f"Hello {guests[0]}, I would like to invite you to dinner.")
+print(f"Hello {guests[1]}, I would like to invite you to dinner.")
+print(f"Hello {guests[2]}, I would like to invite you to dinner.")
+print(f"Hello {guests[3]}, I would like to invite you to dinner.")
