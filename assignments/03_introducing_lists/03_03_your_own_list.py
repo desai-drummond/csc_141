b6934af0d57611b3a4396f519c824eaf813a2965
desai-drummond <desai.drummond001@albright.edu>
2026-09-17 14:19:24 -0400

    test 3-3.

---
+++ assignments/03_introducing_lists/03_03_your_own_list.py
@@ -0,0 +1,9 @@
+# Desai Drummond
+# Chapter 3 
+
+cars = ["Honda Civic", "Dodge Charger", "Ford Mustang", "Chevy Camaro"]
+
+print(f"I would like to own a {cars[0]}.")
+print(f"I would like to own a {cars[1]}.")
+print(f"I would like to own a {cars[2]}.")
+print(f"I would like to own a {cars[3]}.")
