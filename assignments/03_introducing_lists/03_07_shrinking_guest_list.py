ace47e89eab4fede69ddf6d73b4a7e043d0178f3
desai-drummond <desai.drummond001@albright.edu>
2026-09-18 13:47:53 -0400

    test 3-7.

---
+++ assignments/03_introducing_lists/03_07_shrinking_guest_list.py
@@ -0,0 +1,19 @@
+# Desai Drummond
+# Chapter 3
+
+guests = ["Kevin Durant", "Michael Jordan", "Kobe Bryant", "Lebron James", "Stephen Curry", "Giannis Antetokounmpo"]
+print("I found out my new dinner table won't arrive in time.")
+print("I can only invite two guests for dinner.\n")
+
+while len(guests) > 2:
+    removed_guest = guests.pop()
+    print(f"Sorry {removed_guest}, I can't invite you to dinner.")
+
+print("\n" + guests[0] + ", you're still invited to dinner.")
+print(guests[1] + ", you're still invited to dinner.")
+
+del guests[1]
+del guests[0]
+
+print("\nFinal guest list:")
+print(guests)
