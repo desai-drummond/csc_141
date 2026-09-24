c361fa88d585211ec8edf72e98e04e99c0b014de
desai-drummond <desai.drummond001@albright.edu>
2026-09-24 12:40:02 -0400

    test 4-3.

---
+++ assignments/04_working_with_lists/04_03_counting_to_twenty.py
@@ -0,0 +1,9 @@
+# Desai Drummond
+# Chapter 4 counting from one to twenty 
+
+for number in range(1, 21):
+    print(number)
+
+print("Finished counting to twenty!")
+print("All done!")
+print("Counting complete!")
