71d3f56cad0f93d80d2e6587177f5451215f16df
desai-drummond <desai.drummond001@albright.edu>
2026-09-24 13:09:32 -0400

    test 4-10.

---
+++ assignments/04_working_with_lists/04_10_slice.py
@@ -0,0 +1,16 @@
+# Desai Drummond 
+# Chapter 4 slice
+
+# Create a list of numbers from 1 to 10
+numbers = list(range(1, 11))
+
+# Print the first three numbers using slicing
+print(numbers[:3])
+
+# Print the middle three numbers using slicing
+print(numbers[3:6])
+
+# Print the last three numbers using slicing
+print(numbers[-3:])
+
+# End of program
