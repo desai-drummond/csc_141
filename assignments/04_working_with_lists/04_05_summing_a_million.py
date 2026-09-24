a54fafeb97e0edc1c2b850e2af633dc63d3dcdb6
desai-drummond <desai.drummond001@albright.edu>
2026-09-24 12:49:21 -0400

    test 4-5.

---
+++ assignments/04_working_with_lists/04_05_summing_a_million.py
@@ -0,0 +1,22 @@
+# Desai Drummond
+# Chapter 4 summing a million
+
+# Create a list of numbers from 1 to one million
+numbers = list(range(1, 1000001))
+
+
+# Use min() to check the smallest number
+print(f"Minimum: {min(numbers)}")
+
+# Use max() to check the largest number
+print(f"Maximum: {max(numbers)}")
+
+# Use sum() to add all the numbers
+total = sum(numbers)
+print(f"Sum: {total}")
+
+
+
+
+
+
