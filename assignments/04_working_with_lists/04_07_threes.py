fedae99e8082bdc495232348761abf90c1ce8af1
desai-drummond <desai.drummond001@albright.edu>
2026-09-24 12:55:04 -0400

    test 4-7.

---
+++ assignments/04_working_with_lists/04_07_threes.py
@@ -0,0 +1,11 @@
+# Desai Drummond
+# Chapter 4 threes
+
+
+# Create a list of multiples of 3 from 3 to 30 using range() with step 3
+threes = list(range(3, 31, 3))
+
+# Print each multiple of 3 using a for loop
+for number in threes:
+    print(number)
+
