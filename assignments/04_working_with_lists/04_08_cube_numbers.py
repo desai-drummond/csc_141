74775e88d94dc010c0e773ddba9319c6db61ce0b
desai-drummond <desai.drummond001@albright.edu>
2026-09-24 13:00:41 -0400

    test 4-8.

---
+++ assignments/04_working_with_lists/04_08_cube_numbers.py
@@ -0,0 +1,10 @@
+# Desai Drummond 
+# Chapter 4 cube numbers
+
+# Create a list of cube numbers from 1 to 10
+cube_numbers = [x**3 for x in range(1, 11)]
+
+# Print each cube number using a for loop
+for number in cube_numbers:
+    print(number)
+
