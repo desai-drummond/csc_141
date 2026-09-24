d2aa411ecd9bc2ec9272dd0b58389e2dc92efff7
desai-drummond <desai.drummond001@albright.edu>
2026-09-24 12:13:55 -0400

    Test 4-2.

---
+++ assignments/04_working_with_lists/04_02_animals.py
@@ -0,0 +1,15 @@
+# Desai Drummond
+# Chapter 4
+
+# List of favorite animals
+animals = ["Dog", "Rabbit", "Elephant"]
+
+# Print each animal in the list
+for animal in animals:
+    print(animal)
+
+# Print a statement about each animal
+for animal in animals:
+    print(f"I like {animal}.")
+
+print("I really love animals!")
