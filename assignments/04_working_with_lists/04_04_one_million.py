2cde3ca81a412d1c40c67232d4bd11040aa9f0bf
desai-drummond <desai.drummond001@albright.edu>
2026-09-24 12:43:26 -0400

    test 4-4.

---
+++ assignments/04_working_with_lists/04_04_one_million.py
@@ -0,0 +1,12 @@
+# Desai Drummond
+# Chapter 4 one million
+
+# Print numbers from 1 to one million
+numbers = list(range(1, 1000001))
+
+
+for number in numbers: 
+    print(number)
+print("Finished counting to one million!")
+print("All done!")
+print("Counting complete!")
