73e4a5cf33137d08e42c4a3c0b25c5bbef7745cf
desai-drummond <desai.drummond001@albright.edu>
2026-09-24 13:07:37 -0400

    test 4-9.

---
+++ assignments/04_working_with_lists/04_09_cube_comprehension.py
@@ -0,0 +1,11 @@
+# Desai Drummond 
+# Chapter 4 cube comprehension 
+
+# Create a list of cube numbers from 1 to 10 using list comprehension
+cube_numbers = [number**3 for number in range(1, 11)]
+
+# Print each cube number using a for loop
+for number in cube_numbers:
+    print(number)
+
+# End of program
