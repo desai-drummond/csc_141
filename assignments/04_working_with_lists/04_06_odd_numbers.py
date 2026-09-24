aa53b97f7741c99b85cfe9e7cdb0e56cb66d4d34
desai-drummond <desai.drummond001@albright.edu>
2026-09-24 12:53:00 -0400

    test 4-6.

---
+++ assignments/04_working_with_lists/04_06_odd_numbers.py
@@ -0,0 +1,11 @@
+# Desai Drummond
+# Chapter 4 odd numbers
+
+
+# Create a list of odd numbers from 1 to 20 using range()
+odd_numbers = list(range(1, 21, 2))
+
+# Print each odd number using a for loop 
+for number in odd_numbers:
+    print(number)
+
