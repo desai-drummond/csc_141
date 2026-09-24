0ef3442c5ba9930152639d5ec8839143dedcf308
desai-drummond <desai.drummond001@albright.edu>
2026-09-24 12:09:32 -0400

    Test 4-1.

---
+++ assignments/04_working_with_lists/04_01_pizzas.py
@@ -0,0 +1,15 @@
+# Desai Drummond
+# Chapter 4 
+
+# List of favorite pizzas 
+pizzas = ["Cheese", "Beef Pepperoni", "Barbecue Chicken"]
+
+# Print each pizza in the list
+for pizza in pizzas:
+    print(pizza)
+
+# Print a statement about each pizza
+for pizza in pizzas:
+    print(f"I like {pizza} pizza.")
+
+print("I really love pizza!")
